48e45f1b4ac8096fa5107dbc0d622bbb85148d9f
Jake Wharton <jw@squareup.com>
2016-04-26 11:59:41 -0400

    Add license headers to source files.

--- src/test/java/com/jakewharton/fliptables/FlipTableTest.java
+++ src/test/java/com/jakewharton/fliptables/FlipTableTest.java
@@ -1,4 +1,19 @@
-package com.jakewharton.fliptables;
+/*
+ * Copyright 2014 Jake Wharton
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+ package com.jakewharton.fliptables;
 
 import org.junit.Rule;
 import org.junit.Test;
@@ -161,7 +176,7 @@ public class FlipTableTest {
 
   @Test public void rowColumnMismatchThrowsWhenMore() {
     String[] headers = { "The", "Headers" };
-    
+
     exception.expect(IllegalArgumentException.class);
     exception.expectMessage("Row 1's 4 columns != 2 columns");
 
